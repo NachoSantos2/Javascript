f4a96cceb9281190c792d56b30a32c3e512700f1
NachoSantos2 <77507861+NachoSantos2@users.noreply.github.com>
2024-02-24 19:29:58 -0300

    Add files via upload

--- ProyectoFinal/javascript/main.js
+++ ProyectoFinal/javascript/main.js
@@ -56,3 +56,51 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+
+
+let docTitle = document.title;
+window.addEventListener("blur",()=>{
+    document.title = "Volve ☹️";
+})
+window.addEventListener("focus",()=>{
+    document.title = docTitle;
+})
+
+
+
+// Array de raquetas
+var raquetas = [
+    { id: 1, marca: 'PaddleMaster', tipo: 'Profesional', precio: 129.99 },
+    { id: 2, marca: 'SmashTech', tipo: 'Recreativa', precio: 59.99 },
+    { id: 3, marca: 'SpeedyPaddle', tipo: 'Principiante', precio: 39.99 },
+    // ... otras raquetas ...
+];
+
+// Función para mostrar el catálogo de raquetas
+function mostrarCatalogo() {
+    var catalogoDiv = document.getElementById('catalogoRaquetas');
+    var html = '<h2>Catálogo de Raquetas</h2><ul>';
+
+    // Itera sobre el array y crea elementos de lista para cada raqueta
+    raquetas.forEach(function(raqueta) {
+        html += '<li>';
+        html += '<strong>Marca:</strong> ' + raqueta.marca + ', ';
+        html += '<strong>Tipo:</strong> ' + raqueta.tipo + ', ';
+        html += '<strong>Precio:</strong> $' + raqueta.precio.toFixed(2);
+        html += '</li>';
+    });
+
+    html += '</ul>';
+    catalogoDiv.innerHTML = html;
+}
+
+// Llama a la función para mostrar el catálogo al cargar la página
+window.onload = mostrarCatalogo;
+
+
+
+function toggleMenu() {
+    var listaDesplegable = document.querySelector('.lista-desplegable');
+    listaDesplegable.style.display = (listaDesplegable.style.display === 'none' || listaDesplegable.style.display === '') ? 'flex' : 'none';
+}
